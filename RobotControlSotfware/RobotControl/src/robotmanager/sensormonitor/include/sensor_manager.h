42246e14a2aa424a8d71ad6bfe0a460e540b7500
Sangjoo Park <sangjoo.park@gmail.com>
2017-11-02 17:38:00 -0400

    Set up interface between sensor monitor and control manager, and create network thread to send video images.

--- RobotControlSotfware/RobotControl/src/robotmanager/sensormonitor/include/sensor_manager.h
+++ RobotControlSotfware/RobotControl/src/robotmanager/sensormonitor/include/sensor_manager.h
@@ -8,7 +8,19 @@ typedef struct
 	unsigned int laser_right_distance;
 }T_SensorData;
 
-void sensor_manager_main(void);
+typedef struct
+{
+	int wall_forward;	// 1: Wall is existed, 0: Wall is not existed.
+	int wall_right;
+	int wall_left;
+	int hit_wall_forward;	// 1: Robot may hit a wall, 0: Robot may not hit a wall.
+	int hit_wall_right;
+	int hit_Wall_left;
+}T_CellData;
+
+typedef void (*fp_robot_stop)(int);	// Parameter 1: Forward, 2: right, 3: left
+
+void sensor_manager_main(fp_robot_stop fpstop);
 T_SensorData get_sensor_data(void);
 
 
