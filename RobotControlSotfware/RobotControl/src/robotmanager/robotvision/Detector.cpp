8a391babd03db719bb5384fd08fad7e1bb0921ec
kt103 <kyutae.cho@gmail.com>
2017-11-12 14:30:41 -0400

    OTSU Binarization 추가

--- RobotControlSotfware/RobotControl/src/robotmanager/robotvision/Detector.cpp
+++ RobotControlSotfware/RobotControl/src/robotmanager/robotvision/Detector.cpp
@@ -79,8 +79,8 @@ bool Detector::findGoalArea(cv::Mat& CameraImage, bool bDebug)
 
 	//const float thresBlueAreaOfROI = 0.2;  // blue point ���� ���� // need tuning
 
-	//Rect RoiRec(10, 3 * CameraImage.rows / 4, CameraImage.cols - 20, CameraImage.rows / 12); //Define region of interest rectangle
-	Rect RoiRec(10, 2 * CameraImage.rows / 3, CameraImage.cols - 20, CameraImage.rows / 12);
+	Rect RoiRec(10, 3 * CameraImage.rows / 4, CameraImage.cols - 20, CameraImage.rows / 12); //Define region of interest rectangle
+	//Rect RoiRec(10, 2 * CameraImage.rows / 3, CameraImage.cols - 20, CameraImage.rows / 12);
 	//Rect RoiRec(10, 10, CameraImage.cols - 20, CameraImage.rows / 12); //Define region of interest rectangle
 
 	Mat roi(CameraImage, RoiRec); // clip image to region of interest 
@@ -316,10 +316,11 @@ float Detector::FindLineInImageAndComputeOffsetAndWidth(cv::Mat& CameraImage, in
 
 	threshold(blur, thresh, m_thresBinary, 255, THRESH_BINARY_INV | THRESH_BINARY); //Color thresholding makes image more blacka nd white
 
-	IplImage tmpimg = thresh;
-	int cnt = cvCountNonZero(&tmpimg);  // black pixel
+	//IplImage tmpimg = thresh;
+	//int cnt = cvCountNonZero(&tmpimg);  // black pixel
+	//cout << "pixel: " << cnt << endl; 
 
-	if(cnt<1000) threshold(blur, thresh, 90, 255, THRESH_BINARY_INV | THRESH_BINARY);
+	//if(cnt<1000) threshold(blur, thresh, 90, 255, THRESH_BINARY_INV | THRESH_BINARY);
 
 	Mat erodeElmt = getStructuringElement(MORPH_RECT, Size(3, 3));
 	Mat dilateElmt = getStructuringElement(MORPH_RECT, Size(5, 5));
@@ -351,6 +352,107 @@ float Detector::FindLineInImageAndComputeOffsetAndWidth(cv::Mat& CameraImage, in
 			//line( CameraImage,Point(cx+10,0),Point(cx+10,CameraImage.rows),Scalar( 0, 255, 0 ),2,8); 
 			if (r.width > tmpWidth)
 			//if (cx > minMaxCx)
+			{
+				minMaxCx = cx;
+				selected_edge = show;
+				selected_edge.x = show.x + r.width - 10;
+				selected_edge.width = 20;
+				nav_point = show;
+				nav_point.x = 10 + minMaxCx - 10;
+				nav_point.width = 20;
+
+				tmpWidth = r.width;
+			}
+		}
+	}
+	selectedWidth = tmpWidth;
+
+	if (fabs(minMaxCx) == DBL_MAX) minMaxCx = roi.cols / 2;
+
+	offsetfromcenter = 1.0f - 2.0f*(float)minMaxCx / roi.cols;
+
+	if (bDebug)
+	{
+		rectangle(CameraImage, selected_edge, SELECT_COLOR, 3);
+		rectangle(CameraImage, nav_point, NAV_COLOR, 3);
+		line(CameraImage, Point(CameraImage.cols / 2, 0), Point(CameraImage.cols / 2, CameraImage.rows), Scalar(0, 0, 0), 2, 8);
+		//if (!IsPi3) imshow( "processed", dilateImg );
+
+		sprintf(text, "Nav Value %f", offsetfromcenter);
+		cv::putText(CameraImage, text, cv::Point(10, 50), CV_FONT_HERSHEY_PLAIN, 2, CV_RGB(0, 0, 0), 3);
+
+		if (!IsPi3) imshow("FindLine", CameraImage);
+	}
+
+	return offsetfromcenter + FUDGE_BIAS;
+}
+
+float Detector::FindLineInImageAndComputeOffsetAndWidth_OTSU(cv::Mat& CameraImage, int& selectedWidth, bool bDebug)
+{
+	bool ret = false;
+	//Mat tmp;
+	//tmp = CameraImage.clone();
+	//ret = findGoalArea(tmp,false);
+
+
+	char text[1024];
+	float offsetfromcenter;
+	cv::Scalar mean, stddev;
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	Mat mono, blur, thresh, erodeImg, dilateImg;
+
+	//selectedWidth = 0; 
+
+	Rect RoiRec(10, 2 * CameraImage.rows / 3, CameraImage.cols - 20, CameraImage.rows / 12); //Define region of interest rectangle
+
+	Mat roi(CameraImage, RoiRec); // clip image to region of interest 
+	cvtColor(roi, mono, COLOR_BGR2GRAY);  // coovert image to grayscale 
+										  //meanStdDev(mono, mean, stddev);         // Comppute image mean and stddev -- be to used later if needed
+
+										  //std::cout<<"Variance: "<<stddev.val[0]<<std::endl;
+	GaussianBlur(mono, blur, Size(9, 9), 2, 2); // blur image to remove small irregularities
+												//threshold(blur, thresh, 0, 255, THRESH_BINARY_INV | THRESH_OTSU); //Color thresholding makes image more blacka nd white
+
+												//threshold(blur, thresh, m_thresBinary, 255, THRESH_BINARY_INV | THRESH_BINARY); //Color thresholding makes image more blacka nd white
+	threshold(blur, thresh, 0, 255, THRESH_BINARY_INV | THRESH_OTSU); //Color thresholding makes image more blacka nd white
+
+	//IplImage tmpimg = thresh;
+	//int cnt = cvCountNonZero(&tmpimg);  // black pixel
+	//cout << "pixel: " << cnt << endl;
+
+	//if(cnt<1000) threshold(blur, thresh, 90, 255, THRESH_BINARY_INV | THRESH_BINARY);
+
+	Mat erodeElmt = getStructuringElement(MORPH_RECT, Size(3, 3));
+	Mat dilateElmt = getStructuringElement(MORPH_RECT, Size(5, 5));
+	erode(thresh, erodeImg, erodeElmt);       // reduces noise Extract edges 
+	dilate(erodeImg, dilateImg, dilateElmt);
+
+	findContours(dilateImg, contours, hierarchy, RETR_LIST, CHAIN_APPROX_SIMPLE);// Find the contours of the frame
+
+	if (bDebug) rectangle(CameraImage, RoiRec, ROI_COLOR, 3); // draw region of interest on camera image
+
+	int tmpWidth = 0;
+	double minMaxCx = -DBL_MAX;
+	Rect selected_edge(0, 0, 0, 0); //Edge beging followed
+	Rect nav_point(0, 0, 0, 0);
+	for (unsigned int i = 0; i<contours.size(); i++)  //Find the biggest contour 
+	{
+		Moments mu = moments(contours[i]);
+
+		if (mu.m00 > 100.0) // area threadhold
+		{
+			Rect r = boundingRect(contours[i]);
+			Rect show(r.x + RoiRec.x, r.y + RoiRec.y, r.width, r.height);
+			if (bDebug) rectangle(CameraImage, show, TRACK_COLOR, 3); // Draw contours found
+
+			selected_edge.width = 20;
+			nav_point.width = 20;
+			double cx;
+			cx = r.x + r.width / 2;
+			//line( CameraImage,Point(cx+10,0),Point(cx+10,CameraImage.rows),Scalar( 0, 255, 0 ),2,8); 
+			if (r.width > tmpWidth)
+				//if (cx > minMaxCx)
 			{
 				minMaxCx = cx;
 				selected_edge = show;
